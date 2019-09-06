d54946db7f3cca2fc80ee8a9906e95d09cd8af4a
Luis Ponce de Leon <luis.p@geopagos.com>
2019-09-06 11:23:24 -0300

    Twig

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,12 +4,20 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     public function homepage()
     {
         return new Response('first page');
+    }
+
+    public function show($slug)
+    {
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug))
+        ]);
     }
 }
